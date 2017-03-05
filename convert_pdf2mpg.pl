9fe69b304231ed60701fdfdcb547f5fcb713e407
Albert Vilella <avilella@gmail.com>
2017-03-05 13:25:03 +0000

    init

---
+++ convert_pdf2mpg.pl
@@ -0,0 +1,44 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use Getopt::Long;
+
+my $inputfile;
+my $debug; my $verbose; my $simulate;
+my $cmd; my $ret;
+my $tag='';
+use File::Basename;
+# ($name,$path,$suffix) = fileparse($inputfile,@suffixlist);
+# $name = fileparse($fullname,@suffixlist);
+# $basename = basename($fullname,@suffixlist);
+# $dirname  = dirname($fullname);
+GetOptions(
+           'i|input|inputfile:s'  => \$inputfile,
+           'debug'                => \$debug,
+           'tag:s'                => \$tag,
+           'verbose'              => \$verbose,
+           'simulate'             => \$simulate,
+          );
+
+my @suffixlist = ('.pdf');
+my ($name,$path,$suffix) = fileparse($inputfile,@suffixlist);
+
+# basename=Illumina.NovaSeq.technical.details.and.implications
+# convert -density 400 $basename.pdf $basename.jpg
+# ffmpeg -f image2 -i ${basename}-%d.jpg -y -vf "setpts=500*PTS" $basename.mpg
+
+my $outfile_jpg = "$path$name.jpg";
+$cmd = "convert -density 400 $inputfile $outfile_jpg";
+print STDERR "# $cmd\n";
+$ret = `$cmd`;
+
+my $outfile_mpg = "$path$name.mpg";
+$cmd = "ffmpeg -f image2 -i $path$name\-\%d.jpg -y -vf \"setpts=500*PTS\" $outfile_mpg";
+print STDERR "# $cmd\n";
+$ret = `$cmd`;
+
+print STDERR "outfile_mpg:\n";
+print        "$outfile_mpg\n";
+
+$DB::single=1;1;
+$DB::single=1;1;
