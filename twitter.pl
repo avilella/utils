60f73311b644a4df6e0a3961e2668c4dc3c3ec2d
Albert Vilella <avilella@gmail.com>
2020-04-16 11:09:06 +0100

    adding wordcloud_cli to top 10 keywords

--- twitter.pl
+++ twitter.pl
@@ -52,7 +52,7 @@ my $prev_preprint = 1;
 my $prev_image    = 1;
 my $status_id;
 
-my $doi;
+my $doi; my $keywords;
 while(1) {
   $cmd = "find $dir -maxdepth 1 -mindepth 1 -name \"*$inputfile\" | xargs -r ls -t | head -n 1";
   print STDERR "#$cmd\n" if ($verbose);
@@ -65,22 +65,54 @@ while(1) {
     $ret = `$cmd`; chomp $ret;
     $cmd = "grep doi $txtfile";
     $ret = `$cmd`; chomp $ret;
-    if ($ret =~ /doi\:\ 10\./) {
+    $DB::single=1;1;#??
+    # Bioinformatics Oupjournals
+    if ($ret =~ /Downloaded from https:\/\/academic.oup.com\/bioinformatics\/article-abstract\/doi/) {
+      $ret =~ s/Downloaded from https:\/\/academic.oup.com\/bioinformatics\/article-abstract\/doi/#oupjournals #bioinformatics doi\: https:\/\/doi.org\//;
+    # GigaScience
+    } elsif ($ret =~ /doi\:\ 10\./) {
       $ret =~ s|doi\:\ 10\.|doi\:\ https://doi.org/10\.|;
     }
     $DB::single=1;1;#??
+    # biorxiv medrxiv
     if ($ret =~ /(^.+https\:\/\/doi\..+)[\.\n]/) {
       $doi = $1;
       $doi =~ s/preprint/\#preprint/;
       $doi =~ s/The\ copyright.+//;
+      $doi =~ s/^(.+\/bioinformatics\/btaa\d+)\/.+/$1/;
     }
     $DB::single=1;1;#??
 
-    my $first = $nt->update("$doi");
+    # $cmd = "grep -i keyword $txtfile";
+    # $ret = `$cmd`; chomp $ret;
+    # $DB::single=1;1;#??
+
+    my $pngfile = $txtfile; $pngfile =~ s/\.txt/\.png/;
+    $cmd = "cat $txtfile | wordcloud_cli --imagefile $pngfile --stopwords /media/sf_Downloads/stop-words/stop-words-english1.txt 2>/dev/null | csvtk sort -H -k 2:rn | head -n 10 | csvtk cut -f 1";
+    print STDERR "#$cmd\n";
+    $ret = `$cmd`; chomp $ret;
+    my @keywords = split("\n",$ret);
+    my $str_keywords;
+    foreach my $word (@keywords) {
+      $word =~ s/\-/\_/g;
+      $word =~ s/\s/\_/g;
+      $str_keywords .= "#$word ";
+    }
+
+    my $first = $nt->update("$doi $str_keywords");
     $status_id = $first->{id};
+    print STDERR "\n[$doi $str_keywords]\n";
     print STDERR "\n[$pdffile $status_id]\n";
     $prev_preprint = $pdffile;
     print STDERR ".";
+
+    # my $file_contents = read_file($pngfile , binmode => ':raw');
+    # my $media = $nt->update_with_media({in_reply_to_status_id => $status_id, status => "$doi \@albertvilella", media => [undef, $pngfile, Content_Type => 'image/png', Content => $file_contents]});
+
+    # $status_id = $media->{id};
+    # print STDERR "\n[$status_id]\n";
+    # $prev_image = $pngfile;
+
     sleep 10;
 
   } else {
